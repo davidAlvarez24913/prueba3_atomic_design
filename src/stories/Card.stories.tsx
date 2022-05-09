fa8390f633b211f1f035147c03a3c9a6b2656592
David AAC <daalvarez17@utpl.edu.ec>
2022-05-08 22:57:03 -0500

    resolved stories Card

--- src/stories/Card.stories.tsx
+++ src/stories/Card.stories.tsx
@@ -1,14 +1,65 @@
+import React, {useState, useEffect}  from "react";
+import {Meta} from '@storybook/react'
+import {Card} from './Card'
+import '../index.css'
+const meta: Meta = {
+    title: 'Card',
+    component: Card
+}
 
+interface Data{
+  x:number,
+  y:number
+  }
 
-import { ComponentMeta } from '@storybook/react';
+  const INITIAL_STATE = [
+    {
+      fecha: '08/04/2022',
+      ventas: 40
+    },
+    {
+      fecha: '07/04/2022',
+      ventas: 32
+    },
+    {
+      fecha: '08/04/2022',
+      ventas: 40
+    },
+    {
+      fecha: '12/04/2022',
+      ventas: 342
+    },
+    {
+      fecha: '16/04/2022',
+      ventas: 123
+    },
+    {
+      fecha: '22/04/2022',
+      ventas: 32
+    }
+  ]
+  const aux = INITIAL_STATE.map((d) => {
+    return ({
+      x: parseInt(d.fecha.slice(0, 2)),
+      y: d.ventas
+    })
+  })
+  
 
-import { Card } from './Card';
+export default meta;
 
-export default {
-  /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Card',
-  component: Card,
-} as ComponentMeta<typeof Card>;
+export const Default = () => {
+    const [data, setData] = useState<Array<Data>>([])
+
+    useEffect(() => {
+      setData(data)
+    })
+
+    return(
+        <div className = 'App'>
+         <h1>Datos</h1>
+            <Card data={aux}/>
+        </div>
+    )
+
+}
